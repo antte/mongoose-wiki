593d6ee96463ff114711c52386e7a37726834853
Andreas Fliesberg <andreas.fliesberg@gmail.com>
2010-10-25 14:28:05 +0200

    Search articles, by title first and then body

--- mongoose_wiki/article/views.py
+++ mongoose_wiki/article/views.py
@@ -15,3 +15,24 @@ def view(request, articleTitle):
     c = Context({"article" : article})
     
     return HttpResponse(t.render(c))
+
+def searchResults(request):
+    
+    query = ""
+    
+    if request.POST['q'] is not None:
+        query = request.POST['q']
+    
+    article = Article()
+    
+    results = article.search(query)
+    
+    #Tests if results is an iterable object
+    if not getattr(results, '__iter__', False):
+        results = []
+    
+    t = loader.get_template("searchResults.html")
+    c = Context({"results" : results})
+    
+    return HttpResponse(t.render(c))
+    
